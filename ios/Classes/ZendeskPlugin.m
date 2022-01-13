2a34585aaad2c829a00a7ec3c3f3b75cc934b54d
Navoda Sathsarani <navoda@fexcon.com>
2022-01-13 13:14:37 +0530

    register with app id

--- ios/Classes/ZendeskPlugin.m
+++ ios/Classes/ZendeskPlugin.m
@@ -29,7 +29,8 @@
 - (void)initialize:(nonnull InitializeRequest *)input error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
    // [ZDKChat initializeWithAccountKey:input.accountKey appId:input.appId queue:dispatch_get_main_queue()];
   //  [ZDKChat registerPushToken:input.deviceToken];
-    [ZDKChat initializeWithAccountKey:input.accountKey queue:dispatch_get_main_queue()];
+     [ZDKChat initializeWithAccountKey:input.accountKey appId:input.appId queue:dispatch_get_main_queue()];
+    //[ZDKChat initializeWithAccountKey:input.accountKey queue:dispatch_get_main_queue()];
 }
 
 - (void)setDepartment:(nonnull SetDepartmentRequest *)input error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
